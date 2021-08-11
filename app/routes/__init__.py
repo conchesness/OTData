ea8aab93fac8f5e0a83d805579718cf95a774c9a
conchesness <37635142+conchesness@users.noreply.github.com>
2021-08-11 12:41:39 -0700

    Move zscripts out of app

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -3,7 +3,6 @@ from .posts import *
 from .calendar import *
 from .feedback import *
 from .scopes import *
-from .zscripts import *
 from .students import *
 from .msgs import *
 from .lists import *

0db5042164313f8ad7f3d7111edae0d9d0a9aa37
conchesness <37635142+conchesness@users.noreply.github.com>
2021-11-16 09:20:45 -0800

    fix checkin error when a checkin exists

--- app/routes/checkin.py
+++ app/routes/checkin.py
@@ -90,7 +90,7 @@ def classdash(gclassid):
 
         # nowPacific = nowUTC.astimezone(timezone('US/Pacific'))
         # All dates retrieved from the DB are in UTC
-        if lastCheckIn and lastCheckIn.gclassid and lastCheckIn.createdate.date() == dt.now(pytz.utc).date() and lastCheckIn.gclassid == int(form.gclassid.data):
+        if lastCheckIn and lastCheckIn.gclassid and lastCheckIn.createdate.date() == dt.now(pytz.utc).date() and lastCheckIn.gclassid == gclassid:
             flash('It looks like you already checkedin to that class today? If so, please delete one of the checkins.')
             return redirect(url_for('classdash',gclassid=gclassid))
 

6d9e15dc4faaf570057531991854c905d33b8773
conchesness <37635142+conchesness@users.noreply.github.com>
2023-04-28 17:19:07 -0700

    fix ave calcs

--- app/routes/transcript.py
+++ app/routes/transcript.py
@@ -28,13 +28,13 @@ def transcript():
             grade = tr.find('td',attrs={"data-tcfc":"HIS.GR"})
             year = tr.find('td',attrs={"data-tcfc":"HIS.YR"})
             term = tr.find('td',attrs={"data-tcfc":"HIS.TE"})
-            cc = tr.find('td',attrs={"data-tcfc":"HIS.CC"})
-            cr = tr.find('td',attrs={"data-tcfc":"HIS.CR"})
             mark = tr.find('td',attrs={"data-tcfc":"HIS.MK"})
             course = tr.find('td',attrs={"data-tcfc":"CRS.CO"})
             otherCourse = tr.find('td',attrs={"data-tcfc":"HIS.CO"})
             cp = tr.find('td',attrs={"data-tcfc":"CRS.CP"})
             nh = tr.find('td',attrs={"data-tcfc":"CRS.NA"})
+            cc = tr.find('td',attrs={"data-tcfc":"HIS.CC"})
+            cr = tr.find('td',attrs={"data-tcfc":"HIS.CR"})
 
             if grade:
                 txtCourse = course.text.strip()
@@ -48,17 +48,15 @@ def transcript():
                     'grade':grade.text.strip(),
                     'year':year.text.strip(),
                     'term':term.text.strip(),
-                    'cc':cc.text.strip(),
-                    'cr':cr.text.strip(),
                     'mark':mark.text.strip(),
                     'course':txtCourse,
                     'altCourse':txtOtherCourse,
                     'cp':cp.text.strip(),
-                    'nh':nh.text.strip()
+                    'nh':nh.text.strip(),
+                    'cc':cc.text.strip(),
+                    'cr':cr.text.strip()
                     }
                 transcript.append(row)
-
-        pd.set_option('display.float_format', '{:.3f}'.format)
 
         transcriptDF = pd.DataFrame.from_dict(transcript)
 
@@ -88,36 +86,48 @@ def transcript():
                             gpDF, 
                             on ='mark', 
                             how ='left')
-        
-        print(transcriptDF)
-        
+                        
         transcriptDF['cc'] = transcriptDF['cc'].astype('float64')
         transcriptDF['cr'] = transcriptDF['cr'].astype('float64')
         # Created an Adjusted Grade Points for Honors and AP whever grade is above a D
         transcriptDF['adjgp'] = np.where(((transcriptDF['nh'] == "H") | (transcriptDF['nh'] == "H/AP")) & ((transcriptDF['gp'] > 1)), transcriptDF['gp']+1, transcriptDF['gp'])
         # Remove values from adjusted grade points that are not cllege prep
         transcriptDF['adjgp'] = np.where((transcriptDF['nh'] == "N") | (transcriptDF['cp'] != "P"), np.nan, transcriptDF['adjgp'])
+        transcriptDF['adjcr'] = np.where(transcriptDF['adjgp'] > 0, transcriptDF['cr'],np.nan)
+        transcriptDF['countcr'] = np.where(transcriptDF['gp'] > 0, transcriptDF['cr'],np.nan)
+
         # Sort the data
         transcriptDF = transcriptDF.sort_values(by=['grade', 'term','snum']).reset_index(drop=True)
         # created weighted colums that multiply the credits received by the gradepoints
-        transcriptDF['weightedgp'] = transcriptDF['cc']*transcriptDF['gp']
-        transcriptDF['weightedadjgp'] = transcriptDF['cc']*transcriptDF['adjgp']
+        transcriptDF['weightedgp'] = transcriptDF['cr']*transcriptDF['gp']
+        transcriptDF['weightedadjgp'] = transcriptDF['cr']*transcriptDF['adjgp']
         # get total for numeric columns
-        transcriptDF.loc['total'] = transcriptDF[['cc','cr','weightedgp','weightedadjgp']].sum()
+        transcriptDF.loc['total'] = transcriptDF[['cc','cr','weightedgp','weightedadjgp','adjcr','countcr']].sum()
         # Divide weighted columns by credit earned (cc) to get weighted averages
 
         transcriptDF.loc['Ave'] = transcriptDF[['gp','adjgp']].mean()
-        transcriptDF.at['Ave','weightedadjgp'] = transcriptDF.at['total','weightedadjgp'] / transcriptDF.at['total','cc']
-        transcriptDF.at['Ave','weightedgp'] = transcriptDF.at['total','weightedgp'] / transcriptDF.at['total','cc']
+        transcriptDF.at['Ave','weightedadjgp'] = transcriptDF.at['total','weightedadjgp'] / transcriptDF.at['total','adjcr']
+        transcriptDF.at['Ave','weightedgp'] = transcriptDF.at['total','weightedgp'] / transcriptDF.at['total','countcr']
 
 
         # Drop the weighted columns
-        #transcriptDF = transcriptDF.drop(['weightedgp','weightedadjgp'],axis=1)
+        # transcriptDF = transcriptDF.drop(['weightedgp','weightedadjgp'],axis=1)
         # Cleanup the NaN's
-        transcriptDF[["sname","snum",'grade','year','term','cc','cr','mark','course','altCourse','cp','nh','gp','adjgp']] = transcriptDF[["sname","snum",'grade','year','term','cc','cr','mark','course','altCourse','cp','nh','gp','adjgp']].fillna('')
+        transcriptDF[["sname","snum",'grade','year','term','cc','cr','mark','course','altCourse','cp','nh']] = transcriptDF[["sname","snum",'grade','year','term','cc','cr','mark','course','altCourse','cp','nh']].fillna('')
         # create the html
-        transcriptDFHTML = Markup(transcriptDF.to_html())
-        transcriptDFHTML = transcriptDFHTML.replace('dataframe','table')
+
+        transcriptDFHTML = transcriptDF.style\
+            .format(precision=2)\
+            .set_table_styles([
+                {'selector': 'tr:hover','props': 'background-color: grey; font-size: 1em;'}])\
+            .set_properties(**{'border': '1px black solid !important'})\
+            .set_table_attributes('class="table"')  \
+            .set_sticky(axis="index")\
+            .set_sticky(axis="columns")\
+            .to_html()
+        
+    
+        transcriptDFHTML = Markup(transcriptDFHTML.replace('dataframe','table'))
 
         return render_template('transcripts/transcript.html', form=form,transcript = transcriptDFHTML,stuName=stuName)
     
